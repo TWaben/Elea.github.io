89e762f90acaa58530aa9f5bc0b8e655c14950dd
TWaben <112811900+TWaben@users.noreply.github.com>
2024-04-17 18:37:00 +0200

    Add files via upload

--- Communication S1.js
+++ Communication S1.js
@@ -1,4 +1,4 @@
-function laisserFaire() {
+function Pourquoi() {
     // Afficher "Faux" pendant 3 secondes
     document.getElementById("resultat").innerText = "Faux";
     setTimeout(function() {
@@ -13,7 +13,22 @@ function laisserFaire() {
     }, 3000);
 }
 
-function donnerTache() {
+function Solution() {
+    // Afficher "Vrai" pendant 3 secondes
+    document.getElementById("resultat").innerText = "Vrai";
+    setTimeout(function() {
+        // Après 3 secondes, afficher le message approprié
+        document.getElementById("resultat").innerText = "En effet, il est important d'organiser des activités structurées et de donner une tâche bien spécifique à un enfant atteint de TSA afin de l'encourager à participer aux travaux de groupe.";
+        // Créer l'élément image et l'ajouter au conteneur
+        var image = document.createElement("img");
+        image.src = "3683132.png";
+        image.alt = "Description de l'image";
+        var imageContainer = document.getElementById("imageContainer");
+        imageContainer.appendChild(image);
+    }, 3000);
+}
+
+function Solution() {
     // Afficher "Vrai" pendant 3 secondes
     document.getElementById("resultat").innerText = "Vrai";
     setTimeout(function() {
