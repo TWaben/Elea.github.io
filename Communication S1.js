6358a294669721dee280a8c0a937f8202d9a9c83
TWaben <112811900+TWaben@users.noreply.github.com>
2024-04-18 15:02:08 +0200

    Add files via upload

--- Communication S1.js
+++ Communication S1.js
@@ -1,44 +1,17 @@
 function Pourquoi() {
-    // Afficher "Faux" pendant 3 secondes
-    document.getElementById("resultat").innerText = "Faux";
-    setTimeout(function() {
-        // Après 3 secondes, afficher le message approprié
-        document.getElementById("resultat").innerText = "En effet, il est important d'organiser des activités structurées et de donner une tâche bien spécifique à un enfant atteint de TSA afin de l'encourager à participer aux travaux de groupe.";
-        // Créer l'élément image et l'ajouter au conteneur
-        var image = document.createElement("img");
-        image.src = "3683132.png";
-        image.alt = "Description de l'image";
-        var imageContainer = document.getElementById("imageContainer");
-        imageContainer.appendChild(image);
-    }, 3000);
+    // Afficher le texte et ajouter la classe pour centrer le texte par rapport au bouton "Pourquoi"
+    document.getElementById("resultat").innerText = "Les difficultés dans les domaines de la communication et des interactions sociales sont l’un des premiers signes cliniques du TSA présent dans le DSM V. Un enfant autiste peut être dans l’incapacité de maintenir une conversation en raison d’une réduction d’intérêts, d’émotions, d’affects et de réponses. L’enfant peut présenter des difficultés dans la compréhension et l’utilisation de la communication verbale ou non verbale.";
+    document.getElementById("resultat").classList.add("resultat-pourquoi");
 }
 
 function Solution() {
-    // Afficher "Vrai" pendant 3 secondes
-    document.getElementById("resultat").innerText = "Vrai";
-    setTimeout(function() {
-        // Après 3 secondes, afficher le message approprié
-        document.getElementById("resultat").innerText = "En effet, il est important d'organiser des activités structurées et de donner une tâche bien spécifique à un enfant atteint de TSA afin de l'encourager à participer aux travaux de groupe.";
-        // Créer l'élément image et l'ajouter au conteneur
-        var image = document.createElement("img");
-        image.src = "3683132.png";
-        image.alt = "Description de l'image";
-        var imageContainer = document.getElementById("imageContainer");
-        imageContainer.appendChild(image);
-    }, 3000);
+    // Afficher le texte
+    document.getElementById("resultat2").innerText = "Dans le cas d’un travail de groupe, l’enseignant peut proposer une tâche bien spécifique à l’enfant afin de cibler son intérêt. Afin de l’investir, le choix d’une tâche en lien avec ses préférences, ses intérêts est essentiel. L’enseignant peut également encourager ses camarades à interagir avec lui.";
+    document.getElementById("resultat2").classList.add("resultat-solution");
 }
 
-function Solution() {
-    // Afficher "Vrai" pendant 3 secondes
-    document.getElementById("resultat").innerText = "Vrai";
-    setTimeout(function() {
-        // Après 3 secondes, afficher le message approprié
-        document.getElementById("resultat").innerText = "En effet, il est important d'organiser des activités structurées et de donner une tâche bien spécifique à un enfant atteint de TSA afin de l'encourager à participer aux travaux de groupe.";
-        // Créer l'élément image et l'ajouter au conteneur
-        var image = document.createElement("img");
-        image.src = "3683132.png";
-        image.alt = "Description de l'image";
-        var imageContainer = document.getElementById("imageContainer");
-        imageContainer.appendChild(image);
-    }, 3000);
+function Savoir() {
+    // Afficher le texte
+    document.getElementById("resultat3").innerText = "A partir de 3 ans, les enfants sont capables d’interagir avec leurs pairs, ils s’engagent dans le jeu et peuvent coopérer.";
+    document.getElementById("resultat3").classList.add("resultat-savoir");
 }
